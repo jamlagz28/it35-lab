a259f5a927179d2753a34d2d3e30b39afa03a148
jamlagz28 <jameslaag2003@gmail.com>
2025-02-06 14:25:43 +0800

    added files

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,12 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'it35-lab',  // Added base URL here
   plugins: [
     react(),
     legacy()
   ],
+  
   test: {
     globals: true,
     environment: 'jsdom',
