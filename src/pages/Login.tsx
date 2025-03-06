bab32dee3ed44f7891df3a49a48eaa8d023fed1a
jamlagz28 <jameslaag2003@gmail.com>
2025-03-07 07:38:42 +0800

    added username,logo and password

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,35 +1,68 @@
 import { 
-    IonButton,
-    IonButtons,
-      IonContent, 
-      IonHeader, 
-      IonMenuButton, 
-      IonPage, 
-      IonTitle, 
-      IonToolbar, 
-      useIonRouter
-  } from '@ionic/react';
-  
-  const Login: React.FC = () => {
-    const navigation = useIonRouter();
+  IonButton,
+  IonAvatar,
+  IonCol,
+  IonContent, 
+  IonGrid, 
+  IonHeader, 
+  IonInput, 
+  IonInputPasswordToggle, 
+  IonPage, 
+  IonRow, 
+  IonTitle, 
+  IonToolbar, 
+  useIonRouter
+} from '@ionic/react';
 
-    const doLogin = () => {
-        navigation.push('/it35-lab/app','forward','replace');
-    }
-    return (
+const Login: React.FC = () => {
+  const navigation = useIonRouter();
+
+  const doLogin = () => {
+      navigation.push('/it35-lab/app', 'forward', 'replace');
+  }
+
+  return (
       <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonTitle>Login</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent className='ion-padding'>
-            <IonButton onClick={() => doLogin()} expand="full">
-                Login
-            </IonButton>
-        </IonContent>
+          <IonHeader>
+              <IonToolbar>
+                  <IonTitle>Login</IonTitle>
+              </IonToolbar>
+          </IonHeader>
+          <IonContent className='ion-padding'>
+              <div style={{ marginTop: '25%' }}>
+                  <IonGrid style={{
+                      display: 'flex',
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                      height: '100%',
+                  }}>
+                      <IonRow>
+                          <IonCol size="8">
+                              <IonAvatar style={{
+                                  display: 'flex',
+                                  alignItems: 'center',
+                                  justifyContent: 'center',
+                              }}>
+                                  <img 
+                                      style={{ width: '100px', objectFit: 'cover' }}
+                                      alt="User Avatar" 
+                                      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRWVkXV1nRBH6hUiuHMuGywaBgZF7PxmtJuw&s" 
+                                  />
+                              </IonAvatar>
+                          </IonCol>
+                      </IonRow>
+                  </IonGrid>
+                  <IonInput label="Username"></IonInput>
+                  <IonInput type="password" label="Password">
+                      <IonInputPasswordToggle slot="end"></IonInputPasswordToggle>
+                  </IonInput>
+                  <IonButton onClick={doLogin} expand="full">
+                      Login
+                  </IonButton>
+              </div>
+          </IonContent>
       </IonPage>
-    );
-  };
-  
-  export default Login;
+  );
+};
+
+export default Login;
