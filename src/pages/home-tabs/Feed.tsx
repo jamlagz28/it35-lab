23a2390b3140693178283ca7c3ebf3a9976ccfd4
jamlagz28 <jameslaag2003@gmail.com>
2025-04-04 09:27:11 +0800

    added changes in pages

--- src/pages/home-tabs/Feed.tsx
+++ src/pages/home-tabs/Feed.tsx
@@ -10,32 +10,41 @@ import {
   IonCardContent,
   IonCardHeader,
   IonCardSubtitle,
-  IonCardTitle
+  IonCardTitle,
+  IonAvatar
 } from '@ionic/react';
 
 const Feed: React.FC = () => {
-return (
-  <IonPage>
-    <IonHeader>
-      <IonToolbar>
-        <IonButtons slot='start'>
-          <IonMenuButton></IonMenuButton>
-        </IonButtons>
-        <IonTitle>Welcome to Spotify</IonTitle>
-      </IonToolbar>
-    </IonHeader>
-    <IonContent fullscreen>
-      <IonCard>
-        <IonCardHeader>
-          <IonCardTitle>Spotify</IonCardTitle>
-          <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
-        </IonCardHeader>
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot='start'>
+            <IonMenuButton></IonMenuButton>
+          </IonButtons>
+          <IonTitle>Welcome to Spotify</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonCard>
+          <IonCardHeader>
+            <IonCardTitle>Spotify</IonCardTitle>
+            <IonCardSubtitle>"Discover New Sounds Every Day"</IonCardSubtitle>
+          </IonCardHeader>
 
-        <IonCardContent>Here's a small text description for the card content. Nothing more, nothing less.</IonCardContent>
-      </IonCard>
-    </IonContent>
-  </IonPage>
-);
+          <IonAvatar style={{ display: 'flex', justifyContent: 'center', marginBottom: '10px' }}>
+            <img 
+              src="https://ritalinboy.com/app/uploads/2022/05/148-1487614_spotify-logo-small-spotify-logo-transparent-hd-png.png" 
+              alt="Spotify Logo" 
+              style={{ width: '100%', height: '100%', objectFit: 'cover' }} 
+            />
+          </IonAvatar>
+
+          <IonCardContent>From relaxing tunes to energetic beats, Spotify offers an endless variety of music to match your mood. Explore now and find your perfect playlist.</IonCardContent>
+        </IonCard>
+      </IonContent>
+    </IonPage>
+  );
 };
 
 export default Feed;
